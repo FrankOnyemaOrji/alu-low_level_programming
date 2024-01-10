e99ff2ed5eba0c11bc7ecc75903ddf6be4bc3faa
FrankOnyemaOrji <f.orji@alustudent.com>
2024-01-10 16:56:38 +0200

    Error in file 6-size.c corrected

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float;
+	int n;
+	n = 2;
 
-	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu bytes(s)\n" (unsigned long)sizeof(c));
-	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	printf("Size of n is: %lu\n" sizeof(n));
 	return (0);
 }
